c5ccf8120a37b59f9406842d84b3e4910658e0f8
Sam Bonnell <sbonnell147@gmail.com>
2025-08-07 18:28:22 -0700

    First commit

---
+++ eigen-buckling/run_buckling_eigen_analysis.py
@@ -0,0 +1,60 @@
+from utils.FiniteElementModel import FiniteElementModel
+from models.IO_buckling_eigen import PanelInput, PanelOutput
+
+UC1 = PanelInput(
+    id = "",
+
+    # Global Geometry
+    num_longitudinal = 1,
+
+    width = 0.6,
+    length = 0.6,
+
+    # Thickness List
+    t_panel = 0.010,
+    t_longitudinal_web = 0.078,
+    t_longitudinal_flange = 0.004,
+
+    # Local stiffener geometry
+    h_longitudinal_web = 0.125,
+    w_longitudinal_flange = 0.100,
+
+    # Applied Pressure
+    axial_force = 10000000,
+
+    # Mesh Settings
+    mesh_plate = 0.05,
+    mesh_longitudinal_web = 0.05,
+    mesh_longitudinal_flange = 0.05
+)
+
+panel = PanelInput(
+    id = "",
+
+    # Global Geometry
+    num_longitudinal = 4,
+
+    width = 3.0,
+    length = 3.0,
+
+    # Thickness List
+    t_panel = 0.010,
+    t_longitudinal_web = 0.078,
+    t_longitudinal_flange = 0.004,
+
+    # Local stiffener geometry
+    h_longitudinal_web = 0.125,
+    w_longitudinal_flange = 0.100,
+
+    # Applied Pressure
+    axial_force = 10000000,
+
+    # Mesh Settings
+    mesh_plate = 0.05,
+    mesh_longitudinal_web = 0.05,
+    mesh_longitudinal_flange = 0.05
+)
+
+fem = FiniteElementModel("models\\buckling_eigen_panel.py", "data\\input.jsonl", "data\\output.jsonl", PanelInput, PanelOutput)
+fem.write(panel)
+fem.run()
